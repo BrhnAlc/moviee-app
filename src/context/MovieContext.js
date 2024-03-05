559d8c74dd57239a8818a63bc74fd43699fc5ea1
burhan <alacaburhan122016@gmail.com>
2024-03-05 13:51:17 +0300

    moviecontext

--- src/context/MovieContext.js
+++ src/context/MovieContext.js
@@ -8,22 +8,23 @@ const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_
 
 const MovieContextProvider = ({ children }) => {
   const [movies, setMovies] = useState([]);
-
+  const [loading, setLoading] = useState(false)
+  
   useEffect(() => {
     getMovies(FEATURED_API);
   }, []);
 
   const getMovies = (API) => {
+    setLoading(true)
     axios
       .get(API)
-      .then((res) => {
-        setMovies(res.data.results);
-      })
-      .catch((err) => console.log(err));
+      .then((res) => setMovies(res.data.results))
+      .catch((err) => console.log(err))
+      .finally(()=>setLoading(false));
   };
 
   return (
-    <MovieContext.Provider value={{ movies }}>
+    <MovieContext.Provider value={{ movies,getMovies,loading }}>
       {children}
     </MovieContext.Provider>
   );
